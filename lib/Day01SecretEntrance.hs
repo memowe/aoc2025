161df21f4078dfebb254abf7e25ade5d2fa58858
Mirko Westermeier <mirko@westermeier.de>
2025-12-01 13:31:17 +0100

    Initial commit

---
+++ lib/Day01SecretEntrance.hs
@@ -0,0 +1,21 @@
+module Day01SecretEntrance where
+
+exampleInput :: String
+exampleInput = """
+L68
+L30
+R48
+L5
+R60
+L55
+L1
+L99
+R14
+L82
+"""
+
+solve_1 :: IO ()
+solve_1 = putStrLn "solve_1"
+
+solve_2 :: IO ()
+solve_2 = putStrLn "solve_2"
