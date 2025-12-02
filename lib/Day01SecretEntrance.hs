89b7fd1f4daf70ba23140b6c5dbf6a018de1fb93
Mirko Westermeier <mirko.westermeier@uni-muenster.de>
2025-12-02 14:08:02 +0100

    Make solve function names unique

--- lib/Day01SecretEntrance.hs
+++ lib/Day01SecretEntrance.hs
@@ -53,8 +53,8 @@ run2 = length . flip evalState def . execWriterT . mapM_ dial . getMoves
         zero    = do  d <- S.get
                       when (d == Dial 0) $ tell one
 
-solve_1 :: String -> IO ()
-solve_1 = print . run1 . read
+solve_01_1 :: String -> IO ()
+solve_01_1 = print . run1 . read
 
-solve_2 :: String -> IO ()
-solve_2 = print . run2 . read
+solve_01_2 :: String -> IO ()
+solve_01_2 = print . run2 . read
